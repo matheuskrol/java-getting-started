a01f699f5c56c9476f5e36f3ca9f7faab528a54b
matheuskrol27 <matheuskrol27@gmail.com>
2018-12-05 18:43:35 -0500

    Teste

--- src/test/java/TesteMedia.java
+++ src/test/java/TesteMedia.java
@@ -7,16 +7,16 @@ public class TesteMedia {
   @Test
   public void Teste1(){
     Media m = new Media();
-    assertEquals(7, m.media(6, 8));
+    assertEquals(7.0, m.media(6.0, 8.0));
   }
   @Test
   public void Teste2(){
     Media m = new Media();
-    assertEquals(3, m.media(1, 5));
+    assertEquals(3.0, m.media(1.0, 5.0));
   }
   @Test
   public void Teste3(){
     Media m = new Media();
-    assertEquals(10, m.media(13, 7));
+    assertEquals(10.0, m.media(13.0, 7.0));
   }
 }
